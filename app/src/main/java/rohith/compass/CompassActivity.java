ff57e431c7f718dbaf6ecc4277bbd70e96094959
Om Rohith <rohithkumar1368@gmail.com>
2017-05-20 18:40:19 +1100

    Initial commit

---
+++ app/src/main/java/rohith/compass/CompassActivity.java
@@ -0,0 +1,13 @@
+package rohith.compass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompassActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_compass);
+	}
+}
